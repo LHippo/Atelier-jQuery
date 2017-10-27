977c0a01f68890907a5dd22f1ebe989719735de5
BaptSlv <baptiste.salvii@gmail.com>
2017-10-27 15:44:56 +0200

    Add files via upload

---
+++ Exo/js/main.js
@@ -0,0 +1,21 @@
+/*------------------------------- Remplasser les '???' par votre code -------------------------------*/
+
+
+//Afficher le formulaire au clic sur 'Login'
+$('???').on('???', () => {
+  $('.login-form').???();
+});
+
+//Dérouler le menu au passage de la souris sur 'Menu'
+$(document).???(() => {
+  const $menuButton = $('???');
+  const $navDropdown = $('???');
+
+  $menuButton.on('???', () => {
+    $navDropdown.show();
+  })
+  
+  $navDropdown.on('???', () => {
+    $navDropdown.???();
+  })
+})
